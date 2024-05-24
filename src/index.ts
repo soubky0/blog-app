ce61d6e1f82d7e48961c2ed3dcba91d6e822789d
soubky0 <omar.soubky@proton.me>
2024-05-24 22:23:30 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
