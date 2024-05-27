108e7623f09aca64d5cc8116abee7943a24c3238
soubky0 <omar.soubky@proton.me>
2024-05-27 23:55:38 +0300

    feat: docker

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,7 +1,9 @@
 import { createClient } from 'redis';
 import logger from "./logger";
 
-const client = createClient();
+const client = createClient({
+    url: process.env.REDIS_URL,
+});
 
 client.on('error', (err) => {
     console.error('Redis client error:', err);
